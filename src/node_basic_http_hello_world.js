8db4ad0fcd4321655a8117971e1b35ea02acfda9
clonn <clonncd@gmail.com>
2012-01-29 08:12:25 +0800

    fixed hello world example source code, Add server variable it is from http.createserver

--- src/node_basic_http_hello_world.js
+++ src/node_basic_http_hello_world.js
@@ -1,12 +1,13 @@
-var ip   = "127.0.0.1",
+var server,
+    ip   = "127.0.0.1",
     port = 1337,
     http = require('http');
 
-http.createServer(function (req, res) {
+server = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 });
 
-http.listen(port, ip);
+server.listen(port, ip);
 
 console.log("Server running at http://" + ip + ":" + port);
